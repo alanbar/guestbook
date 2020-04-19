1272bd48a8c8dbeaf015062d51b252c1cf3957e6
Alan Barnett <alanbar@clear.net.nz>
2020-04-19 19:45:38 +1200

    include login/out

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,43 @@
 <script>
-import Login from "./firebase/Login.svelte"
-import Guestbook from  "./firebase/Guestbook.svelte"
+    import firebase from "firebase/app" 
+    import "firebase/auth"
+    import config from './firebase/config'
+    import Guestbook from  "./firebase/Guestbook.svelte"
+    import Profile from  "./firebase/Profile.svelte"
+
+    if(!firebase.apps.length) {
+        firebase.initializeApp(config) 
+    }
+
+    let user;
+
+    async function login() {
+        const result = await firebase.auth().signInWithPopup(new firebase.auth.GoogleAuthProvider())
+        user = result.user
+       // console.log('u', user)
+    }
+
+    async function logout() {
+        await firebase.auth().signOut()
+        user = null
+    }
 </script>
 
-<main>
-	<Login /> 
-	<Guestbook />
-</main>
+{#if user}
+    <Profile {...user} />
+    <p></p>
+    <button on:click={logout}>Logout</button>
+    <p></p>
+    <Guestbook />
 
+{:else}
+    <button on:click={login}>Login</button>
+{/if}
+
+<!-- <style>
+    .profile img.profile {
+        height: 50px;
+        width: 50px;
+        display: block
+    }
+</style> -->
---
+++ src/firebase/store.js
@@ -0,0 +1,3 @@
+import { writable } from 'svelte/store';
+let storedUser = writable('z')
+export {storedUser}
